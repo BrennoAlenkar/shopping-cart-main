27989a4d1a3157a47427d66e0c08a362f5683dd2
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2025-08-15 10:28:09 -0300

    teste

--- src/api/fetchProducts.js
+++ src/api/fetchProducts.js
@@ -1,14 +1,8 @@
 const fetchProducts = async (query) => {
-  try {
-    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
-    if (!response.ok) {
-      return [];
-    }
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    return [];
-  }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const data = await response.json();
+
+  return data.results;
 };
 
 export default fetchProducts;
